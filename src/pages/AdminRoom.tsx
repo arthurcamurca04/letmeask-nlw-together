338862d3f65dcf28c9d698bd6210044147804328
Arthur Camurça <arthur.04.pb@gmail.com>
2021-06-25 09:49:40 -0300

    Página de Admin

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,77 @@
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { Question } from "../components/Questions";
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { useRoom } from "../hooks/useRoom";
+import { database } from "../services/firebase";
+import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
+
+export const AdminRoom = () => {
+  const { user } = useAuth();
+  const [newQuestion, setNewQuestion] = useState("");
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { title, questions } = useRoom(roomId);
+
+  async function handleSendQuestion(e: FormEvent) {
+    e.preventDefault();
+    if (newQuestion.trim() === "") {
+      return;
+    }
+
+    if (!user) {
+      throw new Error("User not authenticaded");
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user?.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+    await database.ref(`/rooms/${roomId}/questions`).push(question);
+    setNewQuestion("");
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+        <div className="question-list">
+          {questions.map((item) => {
+            return (
+              <Question
+                key={item.id}
+                content={item.content}
+                author={item.author}
+              ></Question>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+};
